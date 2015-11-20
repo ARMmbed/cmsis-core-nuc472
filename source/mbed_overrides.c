18d35ed17d75a909fbdeb2cc6ae71ae632a955dd
cyliangtw <cyliang@nuvoton.com>
2015-11-20 15:34:37 +0800

    first commit

---
+++ source/mbed_overrides.c
@@ -0,0 +1,29 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2015-2016 Nuvoton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//#include "mbed-drivers/mbed_interface.h"
+
+// FIXME
+#if 0
+void mbed_mac_address(char *mac)
+{
+    mac[0] = 0x00;
+    mac[1] = 0x02;
+    mac[2] = 0xF7;
+    mac[3] = 0xF0;
+    mac[4] = 0x00;
+    mac[5] = 0x00;
+}
+#endif
